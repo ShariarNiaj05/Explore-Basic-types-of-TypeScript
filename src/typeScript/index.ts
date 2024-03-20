1141fce1f8ed874c79c399a11fa9a1415423d1c4
Shariar Islam <shariarn85@gmail.com>
2024-03-20 07:26:11 +0600

    Primitive Types In Typescript

--- src/typeScript/index.ts
+++ src/typeScript/index.ts
@@ -1,5 +1,21 @@
-const course: string = "Next level web development..."
+const course: string = "Next level web development...";
 console.log(course);
 
-const nodemonTest: string = "testing if nodemon working or not...."
+const nodemonTest: string = "testing if nodemon working or not....";
 console.log(nodemonTest);
+
+// Primitive Types
+
+/***
+ * string
+ * number
+ * boolean
+ * null
+ * undefined
+ *
+ */
+
+// As we're going to Declare the type is's called "Explicit Type"
+const myName: string = "Shariar";
+const rollNumber: number = 101;
+const isAdmin: boolean = true;
